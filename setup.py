fc8f826d82e73688bcd41d5b3eb892176997e14a
David Winchester <lwinchester@salesforce.com>
2021-09-17 15:17:17 +0200

    Added pytest to setup

--- setup.py
+++ setup.py
@@ -9,19 +9,21 @@ from shutil import rmtree
 from setuptools import find_packages, setup, Command
 
 # Package meta-data.
-NAME = "GCBC HelloWorld 1.0.0"
-DESCRIPTION = "An example of packaging code"
-URL = "https://github.com/goodcodebadcode/pypi-helloworld"
-EMAIL = "david@goodcodebadcode.com"
-AUTHOR = "David Ritchie"
-REQUIRES_PYTHON = ">=3.7.0"
-VERSION = "1.0.0"
+NAME = 'GCBC HelloWorld 1.0.0'
+DESCRIPTION = 'An example of packaging code'
+URL = 'https://github.com/goodcodebadcode/pypi-helloworld'
+EMAIL = 'david@goodcodebadcode.com'
+AUTHOR = 'David Ritchie'
+REQUIRES_PYTHON = '>=3.7.0'
+VERSION = '1.0.0'
+TESTS = 'tests'
 
 # What packages are required for this module to be executed?
 _INSTALL_REQUIRES = [
-    "click",
-    "requests",
-    "PyGithub==1.53.0"
+    'click',
+    'requests',
+    'PyGithub==1.53.0',
+    'pytest-runner'
 ]
 
 # The directory containing this file
@@ -38,11 +40,11 @@ except FileNotFoundError:
 # Load the package's __version__.py module as a dictionary.
 about = {}
 if not VERSION:
-    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
-    with open(os.path.join(here, project_slug, "__version__.py")) as f:
+    project_slug = NAME.lower().replace('-', '_').replace(' ', '_')
+    with open(os.path.join(here, project_slug, '__version__.py')) as f:
         exec(f.read(), about)
 else:
-    about["__version__"] = VERSION
+    about['__version__'] = VERSION
 
 # This call to setup() does all the work
 setup(
@@ -50,22 +52,23 @@ setup(
     version=about['__version__'],
     description=DESCRIPTION,
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     url=URL,
     author=AUTHOR,
     author_email=EMAIL,
-    license="MIT",
+    license='MIT',
     classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
     ],
-    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    packages=find_packages(exclude=['tests', '*.tests', '*.tests.*', 'tests.*']),
     include_package_data=True,
     install_requires=_INSTALL_REQUIRES,
+    test_suite=TESTS,
     entry_points={
-        "console_scripts": [
-            "pypi-helloworold=helloworld.helloworld:hello_world",
+        'console_scripts': [
+            'pypi-helloworold=helloworld.helloworld:hello_world',
         ]
     },
 )
